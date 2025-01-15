010af336e6721bc46df90091c5d818a19f0c6bd7
Mario Sieg (Neo) <mario.sieg.64@gmail.com>
2025-01-15 14:00:57 +0100

    Fixed BLAS include for amd64

--- magnetron/magnetron_cpu_blas_amd64_avx2.c
+++ magnetron/magnetron_cpu_blas_amd64_avx2.c
@@ -10,4 +10,4 @@
 #define MAG_BLAS_SPECIALIZATION mag_cpu_blas_specialization_amd64_avx2
 #define MAG_BLAS_SPECIALIZATION_FEAT_REQUEST mag_cpu_blas_specialization_amd64_avx2_features
 
-#include "magnetron_cpu_blas.h"
+#include "magnetron_cpu_blas.inl"
--- magnetron/magnetron_cpu_blas_amd64_avx512f.c
+++ magnetron/magnetron_cpu_blas_amd64_avx512f.c
@@ -10,4 +10,4 @@
 #define MAG_BLAS_SPECIALIZATION mag_cpu_blas_specialization_amd64_avx512f
 #define MAG_BLAS_SPECIALIZATION_FEAT_REQUEST mag_cpu_blas_specialization_amd64_avx512f_features
 
-#include "magnetron_cpu_blas.h"
+#include "magnetron_cpu_blas.inl"
